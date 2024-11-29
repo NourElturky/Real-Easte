af9a1454a2416d8564f3cc1ef15a8153eb40406c
Desoky <desoky.web3@gmail.com>
2024-11-29 10:12:40 +0200

    Finish Explore Our Properties

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { Unit } from "./_components/_types/CardTypes";
 import Footer from "./_components/footer/Footer";
 import Section from "./_components/unitCard/SectionWrapper";
+import { Unit } from './_components/_types/CardTypes';
+import { PropertyCard } from './_components/_types/PropertyCard';
+import PropertyGrid from './_components/propertyCard/PropertyGrid';
 
 const cardUnits: Unit[] = [
   {
@@ -332,10 +334,35 @@ const saleProperties: Unit[] = [
   },
 ];
 
+const properties: PropertyCard[] = [
+  {
+    id: 1,
+    title: "New York",
+    count: 8,
+    image: '/images/c1.png',
+    link: "/properties/new-york",
+  },
+  {
+    id: 2,
+    title: "Chicago",
+    count: 2,
+    image: '/images/c2.png',
+    link: "/properties/chicago",
+  },
+  {
+    id: 3,
+    title: "Los Angeles",
+    count: 1,
+    image: '/images/c3.png',
+    link: "/properties/los-angeles",
+  },
+];
+
 const Home: React.FC = () => {
   return (
-    <div>
     <div className="container mx-auto p-4 sm:p-6">
+    <PropertyGrid properties={properties} />
+
       <Section
         title="Discover Our Best Deals"
         units={cardUnits}
@@ -354,7 +381,6 @@ const Home: React.FC = () => {
         gridColumns="grid-cols-1 sm:grid-cols-2 lg:grid-cols-4"
         itemsPerPage={4}
       />
-    </div>
     <Footer />
   </div>
   );
