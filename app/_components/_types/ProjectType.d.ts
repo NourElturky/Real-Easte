12299a3290145a62d2fb00e3a1fdf30592a0057a
Nour Elturky <nourelturky9@gmail.com>
2025-02-03 11:02:41 +0200

    projects card

---
+++ app/_components/_types/ProjectType.d.ts
@@ -0,0 +1,7 @@
+export interface Project {
+    id: number;
+    title: string;
+    count: number;
+    image: string;
+    link: string;
+}
---
+++ app/_components/projectsCard/projectsCard.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Image from "next/image";
+import { Project } from "../_types/ProjectType";
+interface ProjectsCardProps {
+  project: Project;
+  index: number;
+}
+const ProjectsCard: React.FC<ProjectsCardProps> = ({ project, index }) => {
+  return (
+    <a
+      href={project.link}
+      key={project.id}
+      className={`relative rounded-[16px] overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300 ${
+        index === 0 ? "col-span-2" : "col-span-1"
+      } h-[395px]`}
+    >
+      <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-black/80 via-black/10 to-transparent" />
+      <Image
+      width={300}
+      height={300}
+        src={project.image}
+        alt={project.title}
+        className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-300"
+      />
+      <div className="absolute top-0 left-0 mt-[45px] ml-[50px] text-white">
+        <p>{project.count} Properties</p>
+        <p className="text-xl font-medium">{project.title}</p>
+      </div>
+    </a>
+  );
+};
+export default ProjectsCard;
