79687f349d99fffef3f812ac0b2693477067d9b3
Nour Elturky <nourelturky9@gmail.com>
2025-02-20 13:04:35 +0200

    create unit details and create details page

---
+++ app/unit-details/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import UnitDetails from "./UnitDetails";
+
+const Page = () => {
+  return (
+    <div>
+      <UnitDetails/>
+    </div>
+  );
+};
+
+export default Page;
