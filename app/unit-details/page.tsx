1f60eb4b1d72c264eeb5fd22346ca3c91ab0ffec
Nour Elturky <nourelturky9@gmail.com>
2025-02-22 19:12:23 +0200

    creat user form and enhance ui of page

--- app/unit-details/page.tsx
+++ app/unit-details/page.tsx
@@ -3,6 +3,7 @@ import UnitDetails from "./UnitDetails";
 import PropertyDetails from "./PropertyDetails";
 import ProjectDetailsComponent from "../_components/ProjectDetailsComponent/ProjectDetailsComponent";
 import { ProjectDetails } from "../_components/_types/ProjectDetails";
+import UserForm from "./UserForm";
 
 const propertyData = [
   { label: "Unit Code", value: "P103447I1" },
@@ -14,8 +15,10 @@ const propertyData = [
 const projectDetails: ProjectDetails = {
   id: 1,
   developerName: "Tamayoz Developments",
-  description: "A duplex is an amazing piece of architecture that offers a distinctive and adaptable way of living. Due to the coexistence of two unique living spaces within the same structure, it perfectly captures duality and symmetry. This duplex is created by Tamayoz in one of their unique projects The Lark Residence with prices range starting from 8470000 EGP and unit areas starting from 124 SQM in the vibrant neighborhood New Cairo.",
-  developerDescription: " Tamayoz was founded as a Saudi-Egyptian developer in 2008 after setting up a series of real estate projects in Saudi Arabia, such as the University of Al-Batrash and Sheikh Khalid Abd El Aziz University. With its experience, it was able to create a successful set of projects in the Egyptian market in neighborhoods such as Al-Narges, Al-Lotus, and Al-Andalusia. Projects of Tamayoz Real Estate Company in Egypt; Ivory Plaza mall in the new capital.",
+  description:
+    "A duplex is an amazing piece of architecture that offers a distinctive and adaptable way of living. Due to the coexistence of two unique living spaces within the same structure, it perfectly captures duality and symmetry. This duplex is created by Tamayoz in one of their unique projects The Lark Residence with prices range starting from 8470000 EGP and unit areas starting from 124 SQM in the vibrant neighborhood New Cairo.",
+  developerDescription:
+    " Tamayoz was founded as a Saudi-Egyptian developer in 2008 after setting up a series of real estate projects in Saudi Arabia, such as the University of Al-Batrash and Sheikh Khalid Abd El Aziz University. With its experience, it was able to create a successful set of projects in the Egyptian market.",
   image: "/images/img1.png",
 };
 
@@ -23,8 +26,15 @@ const Page = () => {
   return (
     <div className="container mx-auto p-4 sm:p-6">
       <UnitDetails />
-      <PropertyDetails details={propertyData} />
-      <ProjectDetailsComponent project={projectDetails} />
+      <div className="flex flex-col justify-between lg:flex-row gap-6">
+        <div className="flex-1">
+          <ProjectDetailsComponent project={projectDetails} />
+          <PropertyDetails details={propertyData} />
+        </div>
+        <div className="w-[25%]">
+          <UserForm />
+        </div>
+      </div>
     </div>
   );
 };
